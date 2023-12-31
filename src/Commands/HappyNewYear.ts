d28e34393d2a77cbedcc4e6ed2d996a614714788
Hatry1337 <Hatrychannel@gmail.com>
2023-12-31 22:52:04 +0300

    fix hny 2

--- src/Commands/HappyNewYear.ts
+++ src/Commands/HappyNewYear.ts
@@ -6,18 +6,18 @@ export class HappyNewYear extends BOTCommand {
     public name: string = "hny";
     public description: string = "🎄🎄🎄";
 
-    private chats: number[] = [];
+    private chats: Set<number> = new Set<number>();
     private lastHour: number = -1;
 
     public async callbackFn(ctx: CommandContext<Context>, author: ChatMember) {
-        this.chats.push(ctx.chat.id);
+        this.chats.add(ctx.chat.id);
         await ctx.reply("🎄");
     }
 
     public async init() {
         setInterval(() => {
             if(this.lastHour !== new Date().getUTCHours()) {
-                for(let c of this.chats) {
+                for(let c of this.chats.values()) {
                     //sorry for this yandere-dev code. i wanted to write this as fast as i can xd
                     switch (new Date().getUTCHours()) {
                         case 19: {
